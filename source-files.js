b3895fee16ec655c3eb8b2130e2923ef45cb2d6b
Deploy from Travis CI <>
2019-01-23 23:02:51 +0000

    Deploy 8448e672b00687341b72f8d9b34cac9e9586e245 to gh-pages

---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["git2"] = {"name":"","dirs":[],"files":["blame.rs","blob.rs","branch.rs","buf.rs","build.rs","call.rs","cert.rs","commit.rs","config.rs","cred.rs","describe.rs","diff.rs","error.rs","index.rs","lib.rs","merge.rs","message.rs","note.rs","object.rs","odb.rs","oid.rs","oid_array.rs","packbuilder.rs","panic.rs","patch.rs","pathspec.rs","proxy_options.rs","rebase.rs","reference.rs","reflog.rs","refspec.rs","remote.rs","remote_callbacks.rs","repo.rs","revspec.rs","revwalk.rs","signature.rs","stash.rs","status.rs","string_array.rs","submodule.rs","tag.rs","time.rs","transport.rs","tree.rs","treebuilder.rs","util.rs"]};
+sourcesIndex["git2_curl"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["libgit2_sys"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();
